f8262bb1e64887ac2bab9a1c91d8396760ce2f44
masoudarvishian <masoudarvishian@gmail.com>
2024-05-26 10:09:46 +0200

    Added auditing

--- accounts/src/main/java/com/dummybank/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/dummybank/accounts/service/impl/AccountsServiceImpl.java
@@ -33,8 +33,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with the given mobile number: "
                     + customer.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -92,8 +90,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 }
