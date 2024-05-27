824b161c4d3fc63888338d7e691938aaff30b6c3
masoudarvishian <masoudarvishian@gmail.com>
2024-05-27 07:57:30 +0200

    Added detailed documentation for the API

--- accounts/src/main/java/com/dummybank/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/dummybank/accounts/dto/AccountsDto.java
@@ -1,18 +1,32 @@
 package com.dummybank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Account",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
+    @Schema(
+            description = "Account number of dummy bank account", example = "1234567890"
+    )
     @NotEmpty
     @Pattern(regexp = "^$|[0-9]{10}", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of dummy bank account", example = "Saving"
+    )
     @NotEmpty(message = "Account type cannot be null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Branch address of dummy bank account", example = "123 New York"
+    )
     @NotEmpty(message = "Branch address cannot be null or empty")
     private String branchAddress;
 }
