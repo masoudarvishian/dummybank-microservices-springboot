cf67d32c49b79d0cdca8f8bb83e98421eebd1b9c
masoudarvishian <masoudarvishian@gmail.com>
2024-06-03 00:03:42 +0200

    Added custom routing to gatewayserver project

--- gatewayserver/src/main/java/com/dummybank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/dummybank/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,37 @@ package com.dummybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator dummyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/dummybank/accounts/**")
+                        .filters(f -> f.rewritePath("/dummybank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/dummybank/loans/**")
+                        .filters(f -> f.rewritePath("/dummybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/dummybank/cards/**")
+                        .filters(f -> f.rewritePath("/dummybank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+    }
 
 }
