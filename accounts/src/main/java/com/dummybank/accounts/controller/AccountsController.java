f5ad67e1d3dd49eaf3f92c2683c38822a778dc7b
masoudarvishian <masoudarvishian@gmail.com>
2024-06-08 16:43:36 +0200

    Added getJavaVersionFallback method for AccountsController

--- accounts/src/main/java/com/dummybank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/dummybank/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.dummybank.accounts.dto.CustomerDto;
 import com.dummybank.accounts.dto.ErrorResponseDto;
 import com.dummybank.accounts.dto.ResponseDto;
 import com.dummybank.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -224,11 +225,18 @@ public class AccountsController {
                     )
             )
     })
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
