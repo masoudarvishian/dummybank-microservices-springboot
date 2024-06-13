a43e7f89bb7e8dc6e921cd8c3ea9857debb6a3c7
masoudarvishian <masoudarvishian@gmail.com>
2024-06-13 20:46:57 +0200

    Added role based authentication

--- gatewayserver/src/main/java/com/dummybank/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/dummybank/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.dummybank.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,14 +22,22 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchange -> exchange
                 .pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/dummybank/accounts/**").authenticated()
-                .pathMatchers("/dummybank/cards/**").authenticated()
-                .pathMatchers("/dummybank/loans/**").authenticated()
-        ).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+                .pathMatchers("/dummybank/accounts/**").hasRole("ACCOUNTS")
+                .pathMatchers("/dummybank/cards/**").hasRole("CARDS")
+                .pathMatchers("/dummybank/loans/**").hasRole("LOANS")
+        ).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor()))
+        );
 
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return serverHttpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
 }
